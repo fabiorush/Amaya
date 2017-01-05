f24aa6ce482652a492a4c15e374f2575b02ce0eb
Fabio Urquiza <fabiorush@gmail.com>
2017-01-05 19:28:17 -0300

    Added missing colourdata.h include

--- Amaya/amaya/wxdialog/PreferenceDlgWX.h
+++ Amaya/amaya/wxdialog/PreferenceDlgWX.h
@@ -8,6 +8,7 @@
 //-----------------------------------------------------------------------------
 #include "wx/notebook.h"
 #include "wx/listbook.h"
+#include <wx/colourdata.h>
 
 #include "AmayaDialog.h"
 #include "MENUconf.h"
